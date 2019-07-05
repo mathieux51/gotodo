8ea9663ded5c0a0a100e74c799688aaf3d5d1f1a
Mathieu <mathieux51@gmail.com>
2019-07-05 08:33:44 +0200

    Update db test coverage

--- db/db_test.go
+++ db/db_test.go
@@ -8,6 +8,8 @@ import (
 func TestGetEnv(t *testing.T) {
 	rh := "redis-host"
 	os.Setenv("REDIS_HOST", rh)
+	defer os.Unsetenv("REDIS_HOST")
+
 	got := getEnv("REDIS_HOST", "127.0.0.1")
 	want := rh
 	if got != want {
@@ -16,9 +18,6 @@ func TestGetEnv(t *testing.T) {
 }
 
 func TestGetEnv_fallback(t *testing.T) {
-	// Init
-	os.Unsetenv("REDIS_HOST")
-
 	got := getEnv("REDIS_HOST", "127.0.0.1")
 	want := "127.0.0.1"
 	if got != want {
@@ -26,13 +25,37 @@ func TestGetEnv_fallback(t *testing.T) {
 	}
 }
 
-// func TestGetStorage(t *testing.T) {
-// 	// Init
-// 	os.Unsetenv("REDIS_HOST")
+func TestGetStorage(t *testing.T) {
+	s, err := NewStorage()
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := s.Conn.Do("PING")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := "PONG"
+	if got != want {
+		t.Errorf("getStorage() = %v, got %v", want, got)
+	}
+}
 
-// 	got := NewStorage()
-// 	want := Storage{}
-// 	if got != want {
-// 		t.Errorf("getStorage() = %v, got %v", want, got)
-// 	}
-// }
+func TestGetStorage_err(t *testing.T) {
+	// Init
+	os.Setenv("REDIS_HOST", "foo")
+	defer os.Unsetenv("REDIS_HOST")
+
+	_, err := NewStorage()
+	if err == nil {
+		t.Fatal(err)
+		t.Error("GetStorage should return an error if REDIS_HOST invalid")
+	}
+}
+
+func TestGetTodoKey(t *testing.T) {
+	want := "todo:1"
+	got := getTodoKey(1)
+	if got != want {
+		t.Errorf("getTodoKey() = %v, got %v", want, got)
+	}
+}
