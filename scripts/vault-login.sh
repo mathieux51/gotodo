1c523fa1029cfdc811bec9440d9c996f09a65ca9
Mathieu <mathieux51@gmail.com>
2020-02-27 11:51:23 +0100

    Update vault-login script

--- scripts/vault-login.sh
+++ scripts/vault-login.sh
@@ -3,17 +3,9 @@
 set -o errexit
 set -o nounset
 
-TOKEN=$(pbpaste)
-# if TOKEN not set ask user input
-if -z $TOKEN
-then
-  echo Enter token:
-  read TOKEN
-else
-  echo Using token from clipboard
-fi
+echo Enter token:
+read TOKEN
 
-echo $TOKEN
-# VAULT_ADDR="http://127.0.0.1:8200"
-# vault login -method=github token=$TOKEN
+VAULT_ADDR="http://127.0.0.1:8200"
+vault login -method=github token=$TOKEN
 
