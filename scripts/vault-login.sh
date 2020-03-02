1a260b27b45406d610340f70660f18da5f98ef77
Mathieu <mathieux51@gmail.com>
2020-03-02 07:47:42 +0100

    Add VAULT_TOKEN to script

--- scripts/vault-login.sh
+++ scripts/vault-login.sh
@@ -8,5 +8,10 @@ read TOKEN
 
 VAULT_ADDR="http://127.0.0.1:8200"
 echo Trying to log to Vault with github token
+unset VAULT_TOKEN
 vault login -method=github token=$TOKEN
+
+# curl \
+#   --request POST \
+#   --data "{\"token\": \"$TOKEN\"}" 'http://127.0.0.1:8200/v1/auth/github/login'
 
